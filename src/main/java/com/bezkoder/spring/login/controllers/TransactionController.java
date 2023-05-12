7b62343d0de4f2a6f1d0ed2a8ad791c31138bf59
mahdielghali <60888933+mahdielghali@users.noreply.github.com>
2023-05-12 17:25:32 +0100

    adding functions to make date objects

--- src/main/java/com/bezkoder/spring/login/controllers/TransactionController.java
+++ src/main/java/com/bezkoder/spring/login/controllers/TransactionController.java
@@ -1,14 +1,17 @@
 package com.bezkoder.spring.login.controllers;
 
 import com.bezkoder.spring.login.models.Client;
+import com.bezkoder.spring.login.models.LastMonths;
 import com.bezkoder.spring.login.models.Transaction;
 import com.bezkoder.spring.login.repository.TransactionRepository;
 import com.bezkoder.spring.login.service.TransactionService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 
@@ -78,18 +81,82 @@ public class TransactionController {
     }
 
     @GetMapping("/lastmonth")
-    public int getLastMonth() {
-        return transactionRepository.transactionLastMonth().size();
+    public String getLastMonth() {
+        Map<String,Integer> res = new HashMap<>();
+        for(int ij= 1; ij<32;ij++){
+            res.put(String.valueOf(ij),0);
+        }
+        for (Transaction trans : transactionRepository.transactionLastMonth()) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(trans.getDate_ajout());
+            res.put(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)),res.get(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)))+1);
+        }
+
+        return  "{"+res.entrySet().stream()
+                .map(e -> "\""+ e.getKey() + "\":\"" + String.valueOf(e.getValue()) + "\"")
+                .collect(Collectors.joining(", "))+"}";
     }
 
     @GetMapping("/lastday")
     public int getLastDay() {
+
+        Map<String,Integer> res = new HashMap<>();
+        for(int ij= 0; ij<24;ij++){
+            res.put(String.valueOf(ij),0);
+        }
+        for (Transaction trans : transactionRepository.transactionLastDay()) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(trans.getDate_ajout());
+            res.put(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)),res.get(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)))+1);
+        }
+
         return transactionRepository.transactionLastDay().size();
     }
 
     @GetMapping("/lastyear")
-    public int getLastYear() {
-        return transactionRepository.transactionLastYear().size();
+    public LastMonths getLastYear() {
+        LastMonths result = new LastMonths();
+
+        for (Transaction trans : transactionRepository.transactionLastYear()){
+            if (trans.getDate_ajout().getMonth()==0){
+                result.january =result.january +1;
+            }
+            if (trans.getDate_ajout().getMonth()==1){
+                result.february =result.february +1;
+            }
+            if (trans.getDate_ajout().getMonth()==2){
+                result.march =result.march +1;
+            }
+            if (trans.getDate_ajout().getMonth()==3){
+                result.april =result.april +1;
+            }
+            if (trans.getDate_ajout().getMonth()==4){
+                result.may =result.may +1;
+            }
+            if (trans.getDate_ajout().getMonth()==5){
+                result.june =result.june +1;
+            }
+            if (trans.getDate_ajout().getMonth()==6){
+                result.july =result.july +1;
+            }
+            if (trans.getDate_ajout().getMonth()==7){
+                result.august =result.august +1;
+            }
+            if (trans.getDate_ajout().getMonth()==8){
+                result.october =result.october +1;
+            }
+            if (trans.getDate_ajout().getMonth()==9){
+                result.september =result.september +1;
+            }
+            if (trans.getDate_ajout().getMonth()==10){
+                result.november =result.november +1;
+            }
+            if (trans.getDate_ajout().getMonth()==11){
+                result.december =result.december +1;
+            }
+        }
+        return result;
+
     }
 
 
---
+++ src/main/java/com/bezkoder/spring/login/models/LastMonths.java
@@ -0,0 +1,16 @@
+package com.bezkoder.spring.login.models;
+
+public class LastMonths {
+    public int january = 0;
+    public int february = 0;
+    public int march = 0;
+    public int april = 0;
+    public int may = 0;
+    public int june = 0;
+    public int july = 0;
+    public int august = 0;
+    public int october = 0;
+    public int september = 0;
+    public int november = 0;
+    public int december = 0;
+}
